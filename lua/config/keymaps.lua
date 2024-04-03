97093650357cbc93d472ad034287516c267046df
Yohann BIGLIA <y.biglia@matawan-mobility.com>
2024-04-03 19:13:13 +0200

    ♻️ Fix keymap LSP

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,8 +2,3 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 local map = LazyVim.safe_keymap_set
-
-map("n", "<leader>fr", "<cmd> Telescope lsp_dynamic_workspace_symbols <cr>", { desc = "Find Workspace symbols" })
-map("n", "<leader>ff", "<cmd> Telescope find_files <cr>", { desc = "Find files" })
-map("n", "<leader>fw", "<cmd> Telescope live_grep<cr>", { desc = "Find grep" })
-map("n", "<leader>fb", "<cmd> Telescope buffers <cr>", { desc = "Find buffers" })
