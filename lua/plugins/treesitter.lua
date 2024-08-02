bdbf69dc55651c0b34e14f0412f1c1cdb38eb585
Yohann BIGLIA <y.biglia@matawan-mobility.com>
2024-08-02 12:08:17 +0200

    ♻️ Update config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    ensure_installed = { "lua" },
+    ensure_installed = { "lua", "php","phpdoc", "angular", "css", "javascript", "json", "scss", "typescript", "vue" },
     highlight = {
       enable = true,
       use_languatree = true,
